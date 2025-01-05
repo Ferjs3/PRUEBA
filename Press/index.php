f0594266935afdfff0893a7dcb0bfa5ada8344ab
Ferjs3 <fernandojs120@gmail.com>
2025-01-05 02:29:34 -0300

    F

---
+++ Press/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+
+<body>
+    <?php include 'header.html'; ?>
+    <div class="container">
+        <div class="hero"></div>
+        <div class="about">
+            <div class="text">
+                <h1>Somos <p class="press">Press</p>
+                </h1>
+                <p>Press se formó en enero del 2022 en el marco del reencuentro musical de un grupo de amigos que habían formado una banda en la adolescencia. En esta ocasión, y con un objetivo más claro debido a la experiencia que cada uno de sus integrantes ganó en otros proyectos, los amigos decidieron encarar una propuesta sólida y diferente dentro de la escena local.
+                <p>Despues de los primeros meses con algunos cambios en la formacion, hoy en dia se encuentran en la etapa de pre produccion para entrar a grabar durante el verano su primer EP, el cual se titulará Histeria.</p>
+                <p>Si bien el sonido predominante de Press son el rock y el pop, también aparecen elementos propios del sonido indie y algunas baladas.</p>
+                </p>
+            </div>
+            <div class="image"></div>
+        </div>
+        <div class="banda">
+            <div class="image2"></div>
+            <div class="integrantes">
+                <h2>Press es:</h2>
+                <p>Luciano Prest (voz y guitarra)</p>
+                <p>Lautaro Gambetta (guitarra y coros)</p>
+                <p>Pablo Iberra (bajo)</p>
+                <p>Gabriel Cacopardo (batería)</p>
+                <p>Martin Zaiden (teclado).</p>
+            </div>
+        </div>
+        
+        <div class="music">
+            <h2>Nuestro ultimo lanzamiento!</h2>
+            <iframe width="560" height="315" src="https://www.youtube.com/watch?v=4VSAl_aHMeY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        </div>
+    </div>
+
+
+    </div>
+    <?php include 'footer.html'; ?>'
+</body>
+
+</html>
